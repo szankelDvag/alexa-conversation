9737231309f2f9702829b50249726b7888e111c1
Zankel, Sebastian /Extern <Sebastian.Zankel@dvag.com>
2018-02-09 13:10:29 +0100

    Babelify

--- request-builder.js
+++ request-builder.js
@@ -2,70 +2,71 @@
 
 const _ = require('underscore');
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 function buildSlots(slots) {
-  const res = {};
-  _.each(slots, (value, key) => {
-    if ( _.isString(value)) {
-      res[key] = {
-        name: key,
-        value: value
-      };
-    } else {
-      res[key] = {
-        ...value,
-        name: key
-      };
-    }
-  });
-  return res;
+    var res = {};
+    _.each(slots, function (value, key) {
+        if (_.isString(value)) {
+            res[key] = {
+                name: key,
+                value: value
+            };
+        } else {
+            res[key] = _extends({}, value, {
+                name: key
+            });
+        }
+    });
+    return res;
 }
 
 function buildSession(e) {
-  return e ? e.sessionAttributes : {};
+    return e ? e.sessionAttributes : {};
 }
 
 function init(options) {
-  let isNew = true;
+    var isNew = true;
 
-  // public API
+    // public API
 
-  const api = {
-    init,
-    build
-  };
-
-  function build(intentName, slots, prevEvent) {
-    if (!options.appId) throw String('AppId not specified. Please run events.init(appId) before building a Request');
-    const res = { // override more stuff later as we need
-      session: {
-        sessionId: options.sessionId,
-        application: {
-          applicationId: options.appId
-        },
-        attributes: buildSession(prevEvent),
-        user: {
-          userId: options.userId,
-          accessToken: options.accessToken
-        },
-        new: isNew
-      },
-      request: {
-        type: 'IntentRequest',
-        requestId: options.requestId,
-        locale: options.locale,
-        timestamp: (new Date()).toISOString(),
-        intent: {
-          name: intentName,
-          slots: buildSlots(slots)
-        }
-      },
-      version: '1.0'
+    var api = {
+        init: init,
+        build: build
     };
-    isNew = false;
-    return res;
-  }
 
-  return api;
+    function build(intentName, slots, prevEvent) {
+        if (!options.appId) throw String('AppId not specified. Please run events.init(appId) before building a Request');
+        var res = { // override more stuff later as we need
+            session: {
+                sessionId: options.sessionId,
+                application: {
+                    applicationId: options.appId
+                },
+                attributes: buildSession(prevEvent),
+                user: {
+                    userId: options.userId,
+                    accessToken: options.accessToken
+                },
+                new: isNew
+            },
+            request: {
+                type: 'IntentRequest',
+                requestId: options.requestId,
+                locale: options.locale,
+                timestamp: new Date().toISOString(),
+                intent: {
+                    name: intentName,
+                    slots: buildSlots(slots)
+                }
+            },
+            version: '1.0'
+        };
+        isNew = false;
+        return res;
+    }
+
+    return api;
 }
 
 module.exports = {init};
